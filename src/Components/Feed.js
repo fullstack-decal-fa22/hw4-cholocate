a4d435895f76a9c31fe2080a06bcb2c0f293aee8
Minh Le <minhle2001@berkeley.edu>
2022-12-14 11:30:22 -0800

    fina;

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -6,16 +6,20 @@ import Block from './Block';
 const Feed = () => {
     /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
     // Refer to Hint 2 for more help!
-
+    const [list, setList] = useState([])
     /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
-
+    const posts = list.map((item) => <Block color = {item} caption = {item}></Block>); // TODO: edit this variable
+    
+    const handleClick = (color) => {
+        setList(posts => [color, ...posts])
+    };
+    
     return (
         <div>
-            <Menu></Menu>
 
-            {/* Below is where all of your Blocks should render! */}
+            <Menu handleClick={handleClick}></Menu>
             {posts}
+
         </div>
     );
 }
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Color from './Color.js'  
 /* Add any imports you think you might need here! */
 
-const Menu = () => { 
-
+const Menu = ({handleClick}) => { 
+    const colorsList = ['red', 'pink', 'purple', 'black'];
     return (
       <div className="colorOptions">
-          {/* TODO */}
+          {colorsList.map((item) => 
+          <Color color = {item} handleClick = {handleClick}></Color>
+          )}
       </div>
     );
 }
